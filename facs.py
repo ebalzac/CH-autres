e3a874f16251536c9d12ad4d63857aa9919c3522
Odysseus <odysseuspolymetis2010@gmail.com>
2017-10-17 15:44:56 +0200

    facs py update

--- facs.py
+++ facs.py
@@ -1,23 +1,22 @@
 
 # coding: utf-8
 
-
 import sys
 import os
 import fnmatch
-from bs4 import BeautifulSoup as soup
 import re
-
+from lxml import etree
 
 path=input('Entrez le chemin de votre dossier source avec "/" final et sans guillemets\n')
+
+ns = {"ns": "http://www.tei-c.org/ns/1.0"}
+
 for idx, fileTemp in enumerate(fnmatch.filter(os.listdir(path), '*.xml')):
     fileTemp=fileTemp.replace("/",":")
-    tei = open(path+fileTemp).read()
-    print("\n"+fileTemp)
-    xmlSoup = soup(tei,"xml")
+    tree = etree.parse(path+fileTemp)
     
-    for idx,elem in enumerate(xmlSoup.find_all('pb',attrs={"facs":True})):
-        tmpFac=elem["facs"]
+    for idx,elem in enumerate(tree.findall('.//ns:pb[@facs]',namespaces=ns)):
+        tmpFac=elem.attrib["facs"]
         match=re.search(r"f[0-9#]+",tmpFac)
         page=match.group()[1:]
         if idx==0:
@@ -25,10 +24,8 @@ for idx, fileTemp in enumerate(fnmatch.filter(os.listdir(path), '*.xml')):
             facs=tmpFac
         if idx>0:
             facs=re.sub(r"f[0-9#]+","f"+str(indexPage+idx),tmpFac)
-        elem["facs"]=facs
-    
-    f = open('/home/odysseus/Bureau/maxime/'+fileTemp, 'w')
-    f.write(xmlSoup.prettify())
-    f.close()     
+        elem.attrib["facs"]=facs
+        
+    tree.write(path+fileTemp, xml_declaration=True,encoding='utf-8')
     print("Fait pour le fichier "+fileTemp)
 
